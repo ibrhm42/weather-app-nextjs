d83ff7f75194012912fccd82c64e5495f55bcec4
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-04 22:08:51 +0500

    add log

--- app/api/weather/hourly/route.ts
+++ app/api/weather/hourly/route.ts
@@ -14,14 +14,16 @@ export async function GET(request: Request) {
   if (!lat || !lon) {
     return Response.json({ message: "Missing parameters" }, { status: 400 })
   }
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=${HOURS}&exclude=current,minutely,daily,alerts&appid=${appid}&units=metric?nocache=${Date.now()}`
-  
-  const res = await fetch(
-    url,
-    {
-      next: { revalidate: 900 },
-    }
+  console.log(
+    "API key from env:",
+    process.env.OPEN_WEATHER_API_KEY ? "exists" : "missing"
   )
+
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=${HOURS}&exclude=current,minutely,daily,alerts&appid=${appid}&units=metric?nocache=${Date.now()}`
+
+  const res = await fetch(url, {
+    next: { revalidate: 900 },
+  })
 
   if (!res.ok) {
     throw new Error("Failed to fetch data")
