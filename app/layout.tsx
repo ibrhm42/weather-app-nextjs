932baf1cccf99a8c18cd735fc428b582871d8139
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-04 20:01:41 +0500

    remove NEXT_PUBLIC from server side API calls

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
         </NextThemesProvider>
         <Script
           strategy={"beforeInteractive"}
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&callback=Function.prototype`}
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places&callback=Function.prototype`}
         />
         <Analytics />
       </body>
