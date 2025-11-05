047166129729b40e88d4a2e4efe0c1f8414a87b8
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-05 20:26:18 +0500

    ABC

--- app/api/weather/hourly/route.ts
+++ app/api/weather/hourly/route.ts
@@ -2,24 +2,14 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const lat = searchParams.get("lat")
   const lon = searchParams.get("lon")
-  const appid = searchParams.get("appid")
   const HOURS = 23
 
-  if (!appid) {
-    return Response.json(
-      { message: "OpenWeather API key not found in environment variables" },
-      { status: 401 }
-    )
-  }
+
   if (!lat || !lon) {
     return Response.json({ message: "Missing parameters" }, { status: 400 })
   }
-  console.log(
-    "API key from env:",
-    process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY ? "exists" : "missing"
-  )
 
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=${HOURS}&exclude=current,minutely,daily,alerts&appid=${appid}&units=metric&nocache=${Date.now()}`
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=${HOURS}&exclude=current,minutely,daily,alerts&appid=183ed297f46a1a3da2019bb9d46afcc4&units=metric&nocache=${Date.now()}`
 
   const res = await fetch(url, {
     next: { revalidate: 900 },
