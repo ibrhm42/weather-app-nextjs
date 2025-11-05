10a026d58380232d2b2a208b5c2ec001cd541f6d
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-05 20:56:10 +0500

    hij

--- actions/getHourlyData.ts
+++ actions/getHourlyData.ts
@@ -5,10 +5,16 @@ export const getHourlyData = async ({
   lat: string
   lon: string
 }) => {
-  const data = await fetch(
-    `https://${process.env.VERCEL_URL}/api/weather/hourly`,
-    { cache: "no-store" }
-  )
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=55.6760968&lon=12.5683371&appid=183ed297f46a1a3da2019bb9d46afcc4`
+
+  const data = await fetch(url,  {
+    cache: "no-store",
+    headers: { "Cache-Control": "no-cache" },
+  })
+  // const data = await fetch(
+  //   `https://${process.env.VERCEL_URL}/api/weather/hourly`,
+  //   { cache: "no-store" }
+  // )
   if (!data.ok) {
     throw new Error("Failed to fetch data")
   }
