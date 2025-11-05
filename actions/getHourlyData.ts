35f22a67a5918acd32f6fb080e69efefa887fa8d
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-05 21:04:08 +0500

    fre

--- actions/getHourlyData.ts
+++ actions/getHourlyData.ts
@@ -5,16 +5,19 @@ export const getHourlyData = async ({
   lat: string
   lon: string
 }) => {
-    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=55.6760968&lon=12.5683371&appid=183ed297f46a1a3da2019bb9d46afcc4`
-
-  const data = await fetch(url,  {
-    cache: "no-store",
-    headers: { "Cache-Control": "no-cache" },
-  })
-  // const data = await fetch(
-  //   `https://${process.env.VERCEL_URL}/api/weather/hourly`,
-  //   { cache: "no-store" }
-  // )
+  const data = await fetch(
+    `https://${process.env.VERCEL_URL}/api/weather/hourly`,
+    {
+      cache: "no-store",
+      headers: {
+        "Cache-Control": "no-cache",
+        // REQUIRED for Vercel internal routing
+        host: process.env.VERCEL_URL || "localhost:3000",
+        "x-forwarded-host": process.env.VERCEL_URL || "localhost:3000",
+        "x-forwarded-proto": "https",
+      },
+    }
+  )
   if (!data.ok) {
     throw new Error("Failed to fetch data")
   }
