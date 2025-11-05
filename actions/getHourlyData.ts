c0e29cfe4aab9fe12a04ca87cdbb67c08cc24135
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-05 19:39:05 +0500

    remove http

--- actions/getHourlyData.ts
+++ actions/getHourlyData.ts
@@ -6,7 +6,7 @@ export const getHourlyData = async ({
   lon: string
 }) => {
   const data = await fetch(
-    `https://${process.env.VERCEL_URL}/api/weather/hourly?lat=${lat}&lon=${lon}&_=${Date.now()}`,
+    `${process.env.VERCEL_URL}/api/weather/hourly?lat=${lat}&lon=${lon}&_=${Date.now()}`,
     {
       cache: "no-store",
     }
